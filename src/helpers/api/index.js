c479278243325332307a5db219d812c3c0cd9224
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-07-27 13:52:29 -0300

    Some settings and Starting front/interface

--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -19,3 +19,40 @@ export {
   getRomanceCategory,
   getDocumentaryCategory,
 };
+
+export const getAll = async () => {
+  return [
+    {
+      title: 'Originais da Netflix',
+      item: await getNetflixShows(),
+    },
+    {
+      title: 'Recomendados',
+      item: await getTrendingAllWeek(),
+    },
+    {
+      title: 'Em Alta',
+      item: await getTopRated(),
+    },
+    {
+      title: 'Ação',
+      item: await getActionCategory(),
+    },
+    {
+      title: 'Comédia',
+      item: await getComedyCategory(),
+    },
+    {
+      title: 'Terror',
+      item: await getTerrorCategory(),
+    },
+    {
+      title: 'Romance',
+      item: await getRomanceCategory(),
+    },
+    {
+      title: 'Documentários',
+      item: await getDocumentaryCategory(),
+    },
+  ];
+};
