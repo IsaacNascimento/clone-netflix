c1fd46c4acac184c73a078048ffae479d317b950
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-01 00:42:57 -0300

    Scroll btn. From left to Right - Card

--- src/Container/components/MovieRow.jsx
+++ src/Container/components/MovieRow.jsx
@@ -2,24 +2,48 @@ import React from "react";
 import "../app/assets/movieRow.css";
 import { ImgApi } from "../../helpers/constantes";
 import { MdNavigateBefore, MdOutlineNavigateNext } from "react-icons/md";
+import { useState } from "react";
 
 export const MovieRow = ({ title, id, items }) => {
-  //   console.log(items);
+  // console.log(items);
+
+  const [scrolX, setScrolX] = useState(-400);
+
+  const handleLeftArrow = () => {
+    let x = scrolX + Math.round(window.innerWidth / 2);
+    console.log(x);
+    if (x > 0) {x = 0};
+    setScrolX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrolX - Math.round(window.innerWidth / 2);
+    const listWidth = items.results.length * 150;
+    console.log(x);
+    console.log(listWidth);
+    console.log(window.innerWidth);
+    if((window.innerWidth - listWidth) > x) x = (window.innerWidth - listWidth) - 60;
+    setScrolX(x);
+  };
+
   return (
     <div className="movieRow">
       <h2 key={id}>{title}</h2>
-      <div className="movieRow--left">
+      <div className="movieRow--left" onClick={handleLeftArrow}>
         <span>
           <MdNavigateBefore />
         </span>
       </div>
-      <div className="movieRow--right">
+      <div className="movieRow--right" onClick={handleRightArrow}>
         <span>
           <MdOutlineNavigateNext />
         </span>
       </div>
       <div className="movieRow--listArea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{ marginLeft: scrolX, width: items.results.length * 150 }}
+        >
           {items.results.length <= 0 && <h1>Carregando...</h1>}
           {items.results.length > 0 &&
             items.results.map((content, index) => (
